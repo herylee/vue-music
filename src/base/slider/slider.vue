5cc2b30ffffdec6cb8f5906c76e71820ab35281c
hery <2940045321@qq.com>
2018-04-26 15:38:30 +0800

    4-26   4-4轮播组件实现（上）  20min左右

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="slider" ref="slider">
+        <div class="slider-group" ref="sliderGroup" >
+            <slot>
+        <!-- 插槽，外部引用的dom会被插入到插槽中 -->
+            </slot>
+         </div>   
+        <div class="dots">
+        </div>
+        </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from "better-scroll";
+import {addClass} from 'common/js/dom'
+
+export default {
+  props: {
+    loop: {
+      type: Boolean,
+      default: true
+    },
+
+    autoplay: {
+      type: Boolean,
+      default: true
+    },
+
+    interval: {
+      type: Number,
+      default: 4000
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this._setSliderWidth();
+      this._initSlider();
+    }, 20);
+  },
+  methods: {
+    _setSliderWidth() {
+      this.children = this.$refs.sliderGroup.children;
+
+      let width=0;
+      let sliderWidth = this.$refs.slider.clientWidth;
+      for(let i=0;i<this.children.length;i++){
+          let child = this.children[i]
+          addClass(child,'slider-item')
+
+          child.style.width = sliderWidth + 'px'
+          width += sliderWidth
+      }
+
+
+        if(this.loop){
+
+            width += 2 * sliderWidth 
+        }
+
+        this.$refs.sliderGroup.style.width = width + 'px'
+    },
+    _initSlider() {}
+  }
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '~common/stylus/variable';
+
+.slider {
+    min-height: 1px;
+
+    .slider-group {
+        position: relative;
+        overflow: hidden;
+        white-space: nowrap;
+
+        .slider-item {
+            float: left;
+            box-sizing: border-box;
+            overflow: hidden;
+            text-align: center;
+
+            a {
+                display: block;
+                width: 100%;
+                overflow: hidden;
+                text-decoration: none;
+            }
+        }
+    }
+}
