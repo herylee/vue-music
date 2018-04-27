242b4a71ca090ccb19e947a78f0bee9537b5b3f1
hery <2940045321@qq.com>
2018-04-27 18:05:56 +0800

    4-27 18:05 4-4 24min

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,22 +1,16 @@
 export function addClass(el, className) {
-/* eslint-disable */
+  console.log(hasClass(el, className))
   if (hasClass(el, className)) {
-
     return
-
   }
-  console.log(hasClass(el,className)) 
+  // console.log(hasClass(el, className))
   let newClass = el.className().split('')
   newClass.push(className)
   el.className = newClass.join('')
-
-
 }
 
 
 export function hasClass(el, className) {
-
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
-
 }
